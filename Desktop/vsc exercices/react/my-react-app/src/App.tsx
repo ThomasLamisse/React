6985c0b45b492b9b34ea55973f38b75dcc973ed7
Thomas Lamisse <tom.lamisse@gmail.com>
2024-10-22 16:22:37 +0200

    components2 pas réussi

--- Desktop/vsc exercices/react/my-react-app/src/App.tsx
+++ Desktop/vsc exercices/react/my-react-app/src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <section>
       <PokemonCard />
+      
     </section>
   );
 }
--- Desktop/vsc exercices/react/my-react-app/src/components/PokemonCard.tsx
+++ Desktop/vsc exercices/react/my-react-app/src/components/PokemonCard.tsx
@@ -1,4 +1,25 @@
 function PokemonCard() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const pokemon = pokemonList[1];
+
+  pokemon.imgSrc ? (
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+      alt=""
+    />
+  ) : (
+    <p>"???"</p>
+  );
   return (
     <section>
       <figure>
