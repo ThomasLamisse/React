82637455d300426480774860aa4ebd2e25e7d780
Thomas Lamisse <tom.lamisse@gmail.com>
2024-10-23 16:33:34 +0200

    props 2

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import PokemonCard from "./components/PokemonCard";
+import "./App.css";
+
+function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  return (
+    <section>
+      <PokemonCard pokemon={pokemonList} />
+    </section>
+  );
+}
+export default App;
